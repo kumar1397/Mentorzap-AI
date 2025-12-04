27f321adc0fedabdc7c3534d7073b13b2b8548dc
kumar1397 <sahuiswar007@outlook.com>
2025-12-04 23:06:48 +0530

    explore page image check

--- app/(root)/explore/explore-client.tsx
+++ app/(root)/explore/explore-client.tsx
@@ -105,7 +105,7 @@ const uniqueDomains = useMemo(() => {
               <Image
                 src={clone.image ?? "/newPic.jpg"}
                 alt={clone.clone_name}
-                className="w-full h-full object-cover object-center"
+                className="w-full h-full object-cover object-[center_20px]"
                 width={100}
                 height={100}
               />
